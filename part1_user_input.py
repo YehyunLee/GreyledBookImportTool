fdebb532d8155884699fec552775e468bb40626f
YehyunLee <glad7cu@gmail.com>
2023-04-24 23:14:06 -0400

    V1

--- part1_user_input.py
+++ part1_user_input.py
@@ -60,7 +60,7 @@ This page is Copyright (c) 2023 Greyled.""")
         try:
             import easygui
             file_path = easygui.fileopenbox(title='Add File', default="*.pdf")
-            st.write(file_path)
+            # st.write(file_path)
 
             # root = tk.Tk()
             # root.withdraw()
